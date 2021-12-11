MarvellEmbeddedProcessors/marvell-dpdk-sdk

commit 6a499b7d79bf06bae3be308a4399bf5ef65b926d

common/cnxk: add telemetry endpoints to SSO
Add telemetry endpoints for SSO.

Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
Change-Id: I27f420ea9139ffddb4bae02cd2a7e352b9e56180

drivers/common/cnxk/cnxk_telemetry_sso.c
@@ -1,0 +1,50 @@
+
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(C) 2021 Marvell.
+ */
+
+#include <unistd.h>
+
+#include "cnxk_telemetry.h"
+#include "roc_api.h"
+#include "roc_priv.h"
+
+static int
+cnxk_tel_sso(struct plt_tel_data *d)
+{
+	struct roc_sso *roc_sso;
+	struct sso *sso;
+
+	roc_sso = idev_sso_get();
+	if (roc_sso == NULL)
+		return SSO_ERR_DEVICE_NOT_BOUNDED;
+
+	sso = roc_sso_to_sso_priv(roc_sso);
+	plt_tel_data_add_dict_ptr(d, "roc_sso", roc_sso);
+	plt_tel_data_add_dict_ptr(d, "sso", sso);
+	plt_tel_data_add_dict_int(d, "max_hws", roc_sso->max_hws);
+	plt_tel_data_add_dict_int(d, "max_hwgrp", roc_sso->max_hwgrp);
+	plt_tel_data_add_dict_int(d, "nb_hws", roc_sso->nb_hws);
+	plt_tel_data_add_dict_int(d, "nb_hwgrp", roc_sso->nb_hwgrp);
+	plt_tel_data_add_dict_int(d, "pf_func", sso->dev.pf_func);
+	plt_tel_data_add_dict_int(d, "pid", getpid());
+
+	return 0;
+}
+
+static int
+cnxk_sso_tel_handle_info(const char *cmd __plt_unused,
+			 const char *params __plt_unused,
+			 struct plt_tel_data *d)
+{
+	plt_tel_data_start_dict(d);
+	cnxk_tel_sso(d);
+	return 0;
+}
+
+PLT_INIT(cnxk_telemetry_sso_init)
+{
+	plt_telemetry_register_cmd(
+		"/cnxk/sso/info", cnxk_sso_tel_handle_info,
+		"Returns sso information. Takes no parameters");
+}

drivers/common/cnxk/meson.build
@@ -78,6 +78,8 @@ includes += include_directories('../../../lib/meter')
 # Telemetry common code
 sources += files('cnxk_telemetry_bphy.c',
                  'cnxk_telemetry_npa.c',
-                 'cnxk_telemetry_nix.c')
+                 'cnxk_telemetry_nix.c',
+                 'cnxk_telemetry_sso.c',
+)
 
 deps += ['bus_pci', 'net', 'telemetry']

drivers/common/cnxk/roc_idev.c
@@ -206,3 +206,23 @@ roc_idev_npa_nix_get(void)
 	dev = container_of(npa_lf, struct dev, npa);
 	return dev->roc_nix;
 }
+
+struct roc_sso *
+idev_sso_get(void)
+{
+	struct idev_cfg *idev = idev_get_cfg();
+
+	if (idev != NULL)
+		return __atomic_load_n(&idev->sso, __ATOMIC_ACQUIRE);
+
+	return NULL;
+}
+
+void
+idev_sso_set(struct roc_sso *sso)
+{
+	struct idev_cfg *idev = idev_get_cfg();
+
+	if (idev != NULL)
+		__atomic_store_n(&idev->sso, sso, __ATOMIC_RELEASE);
+}

drivers/common/cnxk/roc_idev_priv.h
@@ -21,6 +21,7 @@ struct idev_cfg {
 	uint64_t lmt_base_addr;
 	struct roc_bphy *bphy;
 	struct roc_cpt *cpt;
+	struct roc_sso *sso;
 	struct nix_inl_dev *nix_inl_dev;
 	plt_spinlock_t nix_inl_dev_lock;
 };
@@ -39,6 +40,8 @@ uint16_t idev_npa_lf_active(struct dev *dev);
 /* idev sso */
 void idev_sso_pffunc_set(uint16_t sso_pf_func);
 uint16_t idev_sso_pffunc_get(void);
+struct roc_sso *idev_sso_get(void);
+void idev_sso_set(struct roc_sso *sso);
 
 /* idev lmt */
 uint16_t idev_lmt_pffunc_get(void);

drivers/common/cnxk/roc_sso.c
@@ -735,6 +735,7 @@ roc_sso_dev_init(struct roc_sso *roc_sso)
 		link_mem = PLT_PTR_ADD(link_mem, link_map_sz);
 	}
 	idev_sso_pffunc_set(sso->dev.pf_func);
+	idev_sso_set(roc_sso);
 	sso->pci_dev = pci_dev;
 	sso->dev.drv_inited = true;
 	roc_sso->lmt_base = sso->dev.lmt_base;

drivers/common/cnxk/roc_sso_priv.h
@@ -26,6 +26,7 @@ struct sso {
 
 enum sso_err_status {
 	SSO_ERR_PARAM = -4096,
+	SSO_ERR_DEVICE_NOT_BOUNDED = -4097,
 };
 
 enum sso_lf_type {

drivers/common/cnxk/roc_utils.c
@@ -220,6 +220,9 @@ roc_error_msg_get(int errorcode)
 	case NIX_AF_ERR_PTP_CONFIG_FAIL:
 		err_msg = "PTP config failed";
 		break;
+	case SSO_ERR_DEVICE_NOT_BOUNDED:
+		err_msg = "SSO pf/vf not found";
+		break;
 	case UTIL_ERR_FS:
 		err_msg = "file operation failed";
 		break;

commit 0a3ef66f1bd2955e1eb6651b6501aea1d33a48a7

common/cnxk: fix return code in telemetry callback
Telemetry callback for sso info should return actual value
from data processing function.

Fixes: 6a499b7d79bf ("common/cnxk: add telemetry endpoints to SSO")

Signed-off-by: Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
Change-Id: Idb69334b34590a3dcbefcc23f9d1f7a87989b025
Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/67527
Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
Reviewed-by: Jerin Jacob Kollanukkaran <jerinj@marvell.com>

drivers/common/cnxk/cnxk_telemetry_sso.c
@@ -38,8 +38,7 @@ cnxk_sso_tel_handle_info(const char *cmd __plt_unused,
 			 struct plt_tel_data *d)
 {
 	plt_tel_data_start_dict(d);
-	cnxk_tel_sso(d);
-	return 0;
+	return cnxk_tel_sso(d);
 }
 
 PLT_INIT(cnxk_telemetry_sso_init)