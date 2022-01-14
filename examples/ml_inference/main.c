29642c8819ec380ec7a7f0394bf4295dc811560a
Srikanth Yalavarthi <syalavarthi@marvell.com>
2022-01-14 00:13:50 -0800

    examples: added ml inference example application
    - added example for mldev module
    - enabled device configure and close
    
    Change-Id: I81abed9dc8361c284ba5676b1ae1e85685da96b3
    Signed-off-by: Srikanth Yalavarthi <syalavarthi@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/69101
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Shivah Shankar Shankar Narayan Rao <sshankarnara@marvell.com>

---
+++ examples/ml_inference/main.c
@@ -0,0 +1,81 @@
+/* SPDX-License-Identifier: BSD-3-Clause
+ * Copyright(c) 2022 Marvell.
+ */
+
+#include <getopt.h>
+#include <stdio.h>
+
+#include <rte_eal.h>
+#include <rte_mldev.h>
+
+static void
+print_usage(const char *prog_name)
+{
+	printf("***Usage: %s [EAL params]\n", prog_name);
+	printf("\n");
+}
+
+static int
+parse_args(int argc, char **argv)
+{
+	int opt, option_index;
+
+	static struct option lgopts[] = {{"help", 0, 0, 'h'}, {NULL, 0, 0, 0}};
+
+	while ((opt = getopt_long(argc, argv, "h", lgopts, &option_index)) !=
+	       EOF)
+		switch (opt) {
+		case 'h':
+			print_usage(argv[0]);
+			return 0;
+		default:
+			printf("ERROR: Unknown option: -%c\n", opt);
+			return -1;
+		}
+
+	return 0;
+}
+
+int
+main(int argc, char **argv)
+{
+	uint8_t dev_count;
+	uint8_t dev_id;
+	uint8_t i;
+	int ret;
+
+	/* Init EAL */
+	ret = rte_eal_init(argc, argv);
+	if (ret < 0)
+		return 1;
+	argc -= ret;
+	argv += ret;
+
+	/* Parse application arguments (after the EAL args) */
+	ret = parse_args(argc, argv);
+	if (ret < 0) {
+		print_usage(argv[0]);
+		return 1;
+	}
+
+	/* Get total number of ML devices initialized */
+	dev_count = rte_mldev_count();
+
+	/* Configure ML devices */
+	for (dev_id = 0; dev_id < dev_count; dev_id++) {
+		if (rte_mldev_configure(dev_id, NULL) != 0) {
+			printf("Device configuration failed, dev_id = %d\n", dev_id);
+			goto close_dev;
+		}
+	}
+
+close_dev:
+	/* Close ML devices */
+	for (i = 0; i < dev_id; i++)
+		rte_mldev_close(i);
+
+	/* clean up the EAL */
+	rte_eal_cleanup();
+
+	return 0;
+}
