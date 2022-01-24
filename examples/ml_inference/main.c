641d216f640f4b52cc639713494c82c13ba0a037
Srikanth Yalavarthi <syalavarthi@marvell.com>
2022-01-24 06:43:45 -0800

    examples: enable create and destroy in ml app
    - updated ml inference application to support model
    create and destroy
    - added command line arguments to get model name
    
    Change-Id: I8dfe852481bc54e6945553be95f4acc142a97360
    Signed-off-by: Srikanth Yalavarthi <syalavarthi@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/69792
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Shivah Shankar Shankar Narayan Rao <sshankarnara@marvell.com>

--- examples/ml_inference/main.c
+++ examples/ml_inference/main.c
@@ -6,7 +6,28 @@
 #include <stdio.h>
 
 #include <rte_eal.h>
+#include <rte_memzone.h>
 #include <rte_mldev.h>
+
+/* ML constants */
+#define ML_MAX_MODELS 32
+#define ML_ALIGN_SIZE 128
+
+/* ML global variables */
+static uint16_t g_models_counter;
+
+/* ML args structure */
+typedef struct {
+	char models[ML_MAX_MODELS][PATH_MAX];
+} ml_opts_t;
+
+/* ML model variables structure */
+typedef struct {
+	uint8_t model_id;
+} ml_model_vars_t;
+
+static ml_opts_t ml_opts;
+static ml_model_vars_t ml_models[ML_MAX_MODELS];
 
 static void
 print_usage(const char *prog_name)
@@ -20,11 +41,25 @@ parse_args(int argc, char **argv)
 {
 	int opt, option_index;
 
-	static struct option lgopts[] = {{"help", 0, 0, 'h'}, {NULL, 0, 0, 0}};
+	static struct option lgopts[] = {
+		{"model", required_argument, NULL, 'm'},
+		{"help", no_argument, NULL, 'h'},
+		{NULL, 0, NULL, 0}};
 
-	while ((opt = getopt_long(argc, argv, "h", lgopts, &option_index)) !=
+	while ((opt = getopt_long(argc, argv, "m:h", lgopts, &option_index)) !=
 	       EOF)
 		switch (opt) {
+		case 'm':
+			if (g_models_counter >= ML_MAX_MODELS) {
+				fprintf(stderr,
+					"Models Limit Exceeded, MAX %d\n",
+					ML_MAX_MODELS);
+				return -1;
+			}
+			strncpy(ml_opts.models[g_models_counter++], optarg,
+				PATH_MAX - 1);
+			break;
+		case '?':
 		case 'h':
 			print_usage(argv[0]);
 			return 0;
@@ -40,8 +75,14 @@ int
 main(int argc, char **argv)
 {
 	struct rte_mldev_config ml_config;
+	struct rte_mldev_model ml_model;
+	const struct rte_memzone *mz;
+	char str[PATH_MAX] = {0};
 	uint8_t dev_count;
 	uint8_t dev_id;
+	uint16_t idx;
+	uint16_t i;
+	FILE *fp;
 	int ret;
 
 	/* Init EAL */
@@ -67,24 +108,71 @@ main(int argc, char **argv)
 
 	/* Configure ML devices, use only dev_id = 0 */
 	dev_id = 0;
+	ret = 0;
 	ml_config.socket_id = rte_mldev_socket_id(dev_id);
 	if (rte_mldev_configure(dev_id, &ml_config) != 0) {
 		printf("Device configuration failed, dev_id = %d\n", dev_id);
-		goto close_dev;
+		ret = -1;
+		goto exit_cleanup;
 	}
 
 	/* Start device */
-	rte_mldev_start(dev_id);
+	if (rte_mldev_start(dev_id) != 0) {
+		fprintf(stderr, "Device start failed, dev_id = %d\n", dev_id);
+		ret = -1;
+		goto error_close;
+	};
+
+	/* Create models */
+	for (idx = 0; idx < g_models_counter; idx++) {
+		fp = fopen(ml_opts.models[idx], "r+");
+		if (fp == NULL) {
+			fprintf(stderr, "Failed to open file : %s\n",
+				ml_opts.models[idx]);
+			goto error_destroy;
+		}
+		fseek(fp, 0, SEEK_END);
+		ml_model.model_size = ftell(fp);
+		fseek(fp, 0, SEEK_SET);
+
+		snprintf(str, PATH_MAX, "model_%d", idx);
+		ml_model.model_name = str;
+
+		mz = rte_memzone_reserve_aligned(
+			ml_model.model_name, ml_model.model_size,
+			rte_mldev_socket_id(dev_id), 0, ML_ALIGN_SIZE);
+		if (mz == NULL) {
+			fprintf(stderr, "Failed to create memzone: %s\n",
+				ml_model.model_name);
+			goto error_destroy;
+		}
+		ml_model.model_buffer = mz->addr;
+
+		fread(ml_model.model_buffer, 1, ml_model.model_size, fp);
+		fclose(fp);
+
+		if (rte_mldev_model_create(dev_id, &ml_model,
+					   &ml_models[idx].model_id) != 0) {
+			fprintf(stderr, "Error creating model : %s\n",
+				ml_model.model_name);
+			goto error_destroy;
+		}
+	}
+
+error_destroy:
+	for (i = 0; i < idx; i++)
+		rte_mldev_model_destroy(dev_id, ml_models[i].model_id);
 
 	/* Stop device */
 	rte_mldev_stop(dev_id);
 
-close_dev:
+error_close:
 	/* Close ML device */
 	rte_mldev_close(dev_id);
 
+exit_cleanup:
 	/* clean up the EAL */
 	rte_eal_cleanup();
 
-	return 0;
+	return ret;
 }
