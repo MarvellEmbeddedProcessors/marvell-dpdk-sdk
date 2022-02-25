d754ca3c2e9b116873acc53951c2a6da3278e713
Srikanth Yalavarthi <syalavarthi@marvell.com>
2022-02-25 06:34:38 -0800

    examples: enqueue dequeue inference jobs in ml app
    - updated ML application to enqeue and dequeue inference jobs
    - supports to enqueue multiple repetitions through option -r
    
    Change-Id: Ic01c4f09e5e0fb0cf54627f9b8c3099358f65a21
    Signed-off-by: Srikanth Yalavarthi <syalavarthi@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/71766
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Shivah Shankar Shankar Narayan Rao <sshankarnara@marvell.com>

--- examples/ml_inference/main.c
+++ examples/ml_inference/main.c
@@ -6,6 +6,7 @@
 #include <stdio.h>
 
 #include <rte_eal.h>
+#include <rte_mempool.h>
 #include <rte_memzone.h>
 #include <rte_ml.h>
 #include <rte_mldev.h>
@@ -13,18 +14,21 @@
 /* ML constants */
 #define ML_MAX_MODELS 32
 #define ML_ALIGN_SIZE 128
-#define ML_QUEUE_SIZE 64
+#define ML_QUEUE_SIZE 512
+#define ML_OPS_SIZE   64
 
 /* ML global variables */
 static uint16_t g_models_counter;
 static uint16_t g_inputs_counter;
 static uint16_t g_outputs_counter;
+static uint64_t g_inference_repetitions;
 
 /* ML args structure */
 typedef struct {
 	char models[ML_MAX_MODELS][PATH_MAX];
 	char inputs[ML_MAX_MODELS][PATH_MAX];
 	char outputs[ML_MAX_MODELS][PATH_MAX];
+	char repetitions[PATH_MAX];
 } ml_opts_t;
 
 /* ML model variables structure */
@@ -52,15 +56,21 @@ static int
 parse_args(int argc, char **argv)
 {
 	int opt, option_index;
+	char *endptr;
+	int64_t num;
 
 	static struct option lgopts[] = {
 		{"model", required_argument, NULL, 'm'},
 		{"input", required_argument, NULL, 'i'},
 		{"output", required_argument, NULL, 'o'},
+		{"repetitions", required_argument, NULL, 'r'},
 		{"help", no_argument, NULL, 'h'},
 		{NULL, 0, NULL, 0}};
 
-	while ((opt = getopt_long(argc, argv, "m:i:o:h", lgopts,
+	/* Set defaults */
+	g_inference_repetitions = 1;
+
+	while ((opt = getopt_long(argc, argv, "m:i:o:r:h", lgopts,
 				  &option_index)) != EOF)
 		switch (opt) {
 		case 'm':
@@ -93,6 +103,18 @@ parse_args(int argc, char **argv)
 			strncpy(ml_opts.outputs[g_outputs_counter++], optarg,
 				PATH_MAX - 1);
 			break;
+		case 'r':
+			strncpy(ml_opts.repetitions, optarg, PATH_MAX - 1);
+			errno = 0;
+			num = strtol(ml_opts.repetitions, &endptr, 10);
+			if (errno != 0 || *endptr != '\0' || num < 1) {
+				fprintf(stderr,
+					"Invalid Repetitions: %s (< 1)\n",
+					ml_opts.repetitions);
+				return -EINVAL;
+			}
+			g_inference_repetitions = num;
+			break;
 		case '?':
 		case 'h':
 			print_usage(argv[0]);
@@ -108,20 +130,29 @@ parse_args(int argc, char **argv)
 int
 main(int argc, char **argv)
 {
+	struct rte_ml_op *ml_op_enq[ML_OPS_SIZE];
+	struct rte_ml_op *ml_op_deq[ML_OPS_SIZE];
 	struct rte_ml_model_info ml_model_info;
 	struct rte_mldev_config ml_config;
 	struct rte_mldev_qp_conf qp_conf;
 	struct rte_mldev_info dev_info;
 	struct rte_ml_model ml_model;
 	const struct rte_memzone *mz;
+	struct rte_mempool *op_pool;
 	char str[PATH_MAX] = {0};
+	uint32_t nenq_total = 0;
+	uint32_t ndeq_total = 0;
 	uint8_t dev_count;
+	uint32_t nenq = 0;
+	uint32_t ndeq = 0;
 	uint8_t dev_id;
 	uint32_t fsize;
+	uint16_t nops;
 	uint16_t idx;
 	int ret = 0;
 	int err = 0;
 	FILE *fp;
+	int i;
 
 	/* Init EAL */
 	ret = rte_eal_init(argc, argv);
@@ -179,6 +210,12 @@ main(int argc, char **argv)
 		fprintf(stderr, "Device start failed, dev_id = %d\n", dev_id);
 		goto error_close;
 	};
+
+	/* Create op mempool */
+	snprintf(str, PATH_MAX, "ml_op_pool");
+	op_pool = rte_mempool_create(str, ML_OPS_SIZE, sizeof(struct rte_ml_op),
+				     0, 0, NULL, NULL, NULL, NULL,
+				     rte_socket_id(), 0);
 
 	/* Create models */
 	for (idx = 0; idx < g_models_counter; idx++) {
@@ -280,15 +317,57 @@ main(int argc, char **argv)
 		}
 		ml_models[idx].obuff = mz->addr;
 
-		ret = rte_ml_model_unload(dev_id, ml_models[idx].model_id);
-		if (ret != 0) {
-			fprintf(stderr, "Error unloading model : %s\n",
-				ml_model.model_name);
-			goto error_model_destroy;
+		while (true) {
+			/* Enqueue ops */
+			if (g_inference_repetitions - nenq > ML_OPS_SIZE)
+				nops = ML_OPS_SIZE;
+			else
+				nops = g_inference_repetitions - nenq;
+
+			if (nops != 0)
+				rte_mempool_get_bulk(op_pool, (void *)ml_op_enq,
+						     nops);
+
+			for (i = 0; i < nops; i++) {
+				ml_op_enq[i]->model_id =
+					ml_models[idx].model_id;
+				ml_op_enq[i]->isize = ml_models[idx].isize;
+				ml_op_enq[i]->ibuffer = ml_models[idx].ibuff;
+				ml_op_enq[i]->osize = ml_models[idx].osize;
+				ml_op_enq[i]->obuffer = ml_models[idx].obuff;
+			}
+
+			/* Enqueue jobs */
+			if (nenq_total != g_inference_repetitions) {
+				nenq = rte_mldev_enqueue_burst(dev_id, 0,
+							       ml_op_enq, nops);
+				nenq_total += nenq;
+			}
+
+			/* Dequeue jobs */
+			if (ndeq_total != g_inference_repetitions) {
+				ndeq = rte_mldev_dequeue_burst(
+					dev_id, 0, ml_op_deq, ML_OPS_SIZE);
+				ndeq_total += ndeq;
+			}
+
+			if (ndeq != 0)
+				rte_mempool_put_bulk(op_pool, (void *)ml_op_deq,
+						     ndeq);
+
+			if ((nenq_total == g_inference_repetitions) &&
+			    (ndeq_total == g_inference_repetitions))
+				break;
 		}
+		fprintf(stdout, "nenq_total = %d, ndeq_total = %d\n",
+			nenq_total, ndeq_total);
+
+		ret = rte_ml_model_unload(dev_id, ml_models[idx].model_id);
+		if (ret != 0)
+			fprintf(stderr, "Error loading model : %s\n",
+				ml_model.model_name);
 
 error_model_destroy:
-
 		err = rte_ml_model_destroy(dev_id, ml_models[idx].model_id);
 		if (err != 0 && ret == 0)
 			ret = err;
@@ -301,7 +380,6 @@ error_model_destroy:
 					ml_opts.outputs[idx]);
 				ret = -1;
 			}
-
 			fwrite(ml_models[idx].obuff, 1, ml_models[idx].osize,
 			       fp);
 			fclose(fp);
