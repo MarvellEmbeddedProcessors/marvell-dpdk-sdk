6315800ec9cefed60f4f7bd7b9fd004d2cfb88db
Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
2022-03-24 13:31:04 +0530

    common/cnxk: use rte_log in plt_dump macros
    Fix plt_dump macros to make use of RTE_LOG instead of fprintf in
    cnxk driver implementation.
    
    Fixes: ab706fa8256 ("common/cnxk: support bandwidth profile dump")
    Fixes: d5dabf1732d ("common/cnxk: update NIX and NPA dump function")
    
    ci: skip_checkformat skip_checkpatch
    
    Signed-off-by: Gowrishankar Muthukrishnan <gmuthukrishn@marvell.com>
    Change-Id: I7741c971fc0dc0772b33a83107a851c23bf12246
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/73342
    Reviewed-by: Sunil Kumar Kori <skori@marvell.com>
    Reviewed-by: Jerin Jacob Kollanukkaran <jerinj@marvell.com>
    Tested-by: Jerin Jacob Kollanukkaran <jerinj@marvell.com>

--- drivers/common/cnxk/roc_platform.h
+++ drivers/common/cnxk/roc_platform.h
@@ -234,8 +234,8 @@ extern int cnxk_logtype_ree;
 #define plt_info(fmt, args...) RTE_LOG(INFO, PMD, fmt "\n", ##args)
 #define plt_warn(fmt, args...) RTE_LOG(WARNING, PMD, fmt "\n", ##args)
 #define plt_print(fmt, args...) RTE_LOG(INFO, PMD, fmt "\n", ##args)
-#define plt_dump(fmt, ...)      fprintf(stderr, fmt "\n", ##__VA_ARGS__)
-#define plt_dump_no_nl(fmt, ...) fprintf(stderr, fmt, ##__VA_ARGS__)
+#define plt_dump(fmt, ...) RTE_LOG(ERR, PMD, fmt "\n", ##__VA_ARGS__)
+#define plt_dump_no_nl(fmt, ...) RTE_LOG(ERR, PMD, fmt, ##__VA_ARGS__)
 
 /**
  * Log debug message if given subsystem logging is enabled.
