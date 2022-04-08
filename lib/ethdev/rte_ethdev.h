7d2e94654b85c544a2d0e8b3606c2593e519f60b
Vamsi Attunuru <vattunuru@marvell.com>
2022-04-08 18:41:53 +0530

    ethdev: add hard expiry event subtypes
    Patch adds new event subtypes for notifying expiry
    events upon reaching hard packet/byte expiry limits
    
    Signed-off-by: Vamsi Attunuru <vattunuru@marvell.com>
    Change-Id: I35859d8a0c417a3ad8ef554784111edfe116f9ce
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/74446
    Tested-by: Jerin Jacob Kollanukkaran <jerinj@marvell.com>
    Reviewed-by: Jerin Jacob Kollanukkaran <jerinj@marvell.com>

--- lib/ethdev/rte_ethdev.h
+++ lib/ethdev/rte_ethdev.h
@@ -3845,6 +3845,10 @@ enum rte_eth_event_ipsec_subtype {
 	RTE_ETH_EVENT_IPSEC_SA_BYTE_EXPIRY,
 	/** Soft packet expiry of SA */
 	RTE_ETH_EVENT_IPSEC_SA_PKT_EXPIRY,
+	/** Hard byte expiry of SA */
+	RTE_ETH_EVENT_IPSEC_SA_BYTE_HARD_EXPIRY,
+	/** Hard packet expiry of SA */
+	RTE_ETH_EVENT_IPSEC_SA_PKT_HARD_EXPIRY,
 	/** Max value of this enum */
 	RTE_ETH_EVENT_IPSEC_MAX
 };
@@ -3867,6 +3871,8 @@ struct rte_eth_event_ipsec_desc {
 	 * - @ref RTE_ETH_EVENT_IPSEC_SA_TIME_EXPIRY
 	 * - @ref RTE_ETH_EVENT_IPSEC_SA_BYTE_EXPIRY
 	 * - @ref RTE_ETH_EVENT_IPSEC_SA_PKT_EXPIRY
+	 * - @ref RTE_ETH_EVENT_IPSEC_SA_BYTE_HARD_EXPIRY
+	 * - @ref RTE_ETH_EVENT_IPSEC_SA_PKT_HARD_EXPIRY
 	 *
 	 * @see struct rte_security_session_conf
 	 *
