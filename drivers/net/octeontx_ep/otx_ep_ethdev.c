75f2fa89c7b772b1a2ee4befe28b5fd14a9a0dd8
Radha Mohan Chintakuntla <radhac@marvell.com>
2022-04-11 22:08:14 -0700

    net/octeontx_ep: additional support for cnf95n and cnf95o
    Added additional support for cnf95n and cnf95o PCI.
    
    Signed-off-by: Radha Mohan Chintakuntla <radhac@marvell.com>
    Change-Id: Ie999f6ad5c763b73b2b9837af697d06fc71ad066
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/74588
    Tested-by: Jerin Jacob Kollanukkaran <jerinj@marvell.com>
    Reviewed-by: Jerin Jacob Kollanukkaran <jerinj@marvell.com>

--- drivers/net/octeontx_ep/otx_ep_ethdev.c
+++ drivers/net/octeontx_ep/otx_ep_ethdev.c
@@ -172,6 +172,8 @@ otx_ep_chip_specific_setup(struct otx_ep_device *otx_epvf)
 		break;
 	case PCI_DEVID_OCTEONTX2_EP_NET_VF:
 	case PCI_DEVID_CN98XX_EP_NET_VF:
+	case PCI_DEVID_CNF95N_EP_NET_VF:
+	case PCI_DEVID_CNF95O_EP_NET_VF:
 		otx_epvf->chip_id = dev_id;
 		ret = otx2_ep_vf_setup_device(otx_epvf);
 		otx_epvf->fn_list.disable_io_queues(otx_epvf);
@@ -211,7 +213,9 @@ otx_epdev_init(struct otx_ep_device *otx_epvf)
 	if (otx_epvf->chip_id == PCI_DEVID_OCTEONTX_EP_VF)
 		otx_epvf->eth_dev->tx_pkt_burst = &otx_ep_xmit_pkts;
 	else if (otx_epvf->chip_id == PCI_DEVID_OCTEONTX2_EP_NET_VF ||
-		 otx_epvf->chip_id == PCI_DEVID_CN98XX_EP_NET_VF)
+		 otx_epvf->chip_id == PCI_DEVID_CN98XX_EP_NET_VF ||
+		 otx_epvf->chip_id == PCI_DEVID_CNF95N_EP_NET_VF ||
+		 otx_epvf->chip_id == PCI_DEVID_CNF95O_EP_NET_VF)
 		otx_epvf->eth_dev->tx_pkt_burst = &otx2_ep_xmit_pkts;
 	else if (otx_epvf->chip_id == PCI_DEVID_CNXK_EP_NET_VF)
 		otx_epvf->eth_dev->tx_pkt_burst = &otx2_ep_xmit_pkts;
@@ -544,8 +548,10 @@ otx_ep_eth_dev_init(struct rte_eth_dev *eth_dev)
 	otx_epvf->pdev = pdev;
 
 	otx_epdev_init(otx_epvf);
-	if (pdev->id.device_id == PCI_DEVID_OCTEONTX2_EP_NET_VF ||
-	    pdev->id.device_id == PCI_DEVID_CN98XX_EP_NET_VF) {
+	if (otx_epvf->chip_id == PCI_DEVID_OCTEONTX2_EP_NET_VF ||
+	    otx_epvf->chip_id == PCI_DEVID_CN98XX_EP_NET_VF ||
+	    otx_epvf->chip_id == PCI_DEVID_CNF95N_EP_NET_VF ||
+	    otx_epvf->chip_id == PCI_DEVID_CNF95O_EP_NET_VF)
 		if (otx_epvf->sdp_packet_mode == SDP_PACKET_MODE_NIC)
 			otx_epvf->pkind = SDP_OTX2_PKIND_FS24;
 		else
