e1168c38c05d289d3a601b8d04179235ce49ab64
Ashwin Sekhar T K <asekhar@marvell.com>
2022-10-21 15:11:53 +0530

    cnxk-test: mempool_perf: update testing method
    Use a custom mempool binary which is based on the default DPDK
    mempool perf test. This custom test binary will run only minimal
    tests which are required. So this binary can be run to completion
    in CI and the output can be checked after the run to completion.
    
    Signed-off-by: Ashwin Sekhar T K <asekhar@marvell.com>
    Change-Id: Ib5ccef9fa27a8f9166984c0ac5f1a7b56d1576bb
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/88722
    Reviewed-by: Jerin Jacob Kollanukkaran <jerinj@marvell.com>
    Tested-by: Jerin Jacob Kollanukkaran <jerinj@marvell.com>

--- marvell-ci/test/cnxk-tests/mempool_perf/cnxk_mempool_perf.sh
+++ marvell-ci/test/cnxk-tests/mempool_perf/cnxk_mempool_perf.sh
@@ -2,81 +2,85 @@
 # SPDX-License-Identifier: BSD-3-Clause
 # Copyright (C) 2021 Marvell.
 
-set -e
+set -euo pipefail
 
 SUDO=${SUDO:-"sudo"}
-TEST_LOG=$(mktemp)
+TEST_LOG=cnxk_mempool_perf.log
 PREFIX="cmpt"
-TOLERANCE=${TOLERANCE:-5}
-REF_FILE=${REF_FILE:-ref_numbers/cn96xx_rclk2200_sclk1100}
+TOLERANCE=${TOLERANCE:-3}
 SCRIPTPATH="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 
-# Find the cnxk-test application
-if [[ -f $SCRIPTPATH/../../../../app/test/dpdk-test ]]; then
-	# This is running from build directory
-	DPDK_TEST_BIN=$SCRIPTPATH/../../../../app/test/dpdk-test
-elif [[ -f $SCRIPTPATH/../../dpdk-test ]]; then
-	# This is running from install directory
-	DPDK_TEST_BIN=$SCRIPTPATH/../../dpdk-test
+# Find the cnxk_mempool_perf application
+if [[ -f $SCRIPTPATH/cnxk_mempool_perf ]]; then
+	TEST_BIN=$SCRIPTPATH/cnxk_mempool_perf
 else
-	DPDK_TEST_BIN=$(command -v dpdk-test)
-	if [[ -z $DPDK_TEST_BIN ]]; then
-		echo "dpdk-test not found !!"
+	TEST_BIN=$(command -v cnxk_mempool_perf)
+	if [[ -z $TEST_BIN ]]; then
+		echo "cnxk_mempool_perf not found !!"
 		exit 1
 	fi
 fi
 
-function kill_dpdk_test()
-{
-	local test_name="$DPDK_TEST_BIN.*$PREFIX"
-
-	# Issue kill
-	$SUDO pkill --signal SIGKILL -f "$test_name" \
-		&> /dev/null || true
-
-	# Wait until the process is killed
-	sleep 1
-	while (pgrep -f "$test_name"); do
-		sleep 1
-		continue
-	done
-}
-
 function test_cleanup()
 {
 	rm -f $TEST_LOG
-	kill_dpdk_test
 }
-
 trap test_cleanup EXIT
 
 function test_mempool_perf()
 {
+	local ref_file=$1
 	local unbuffer="stdbuf -o0"
-	local pattern='s/mempool_autotest cache=0 cores=1 n_get_bulk=32 n_put_bulk=32 n_keep=128 rate_persec=([0-9]+)/\1/p'
+	local pattern='mempool_autotest cache'
+	local ref_value
 	local expected
 	local result
 
-	expected=$(<$REF_FILE)
+	# Run the cnxk_mempool_perf application
+	$SUDO DPDK_TEST=cnxk_mempool_perf $unbuffer $TEST_BIN \
+		--file-prefix $PREFIX |& tee $TEST_LOG
 
-	# Run test until expected combination is found
-	$SUDO DPDK_TEST=mempool_perf_autotest $unbuffer $DPDK_TEST_BIN \
-		--file-prefix $PREFIX &> $TEST_LOG &
-	while true; do
-		result=$(sed -nr "$pattern" $TEST_LOG | head -n1)
-		if [[ -n $result ]]; then
-			break;
-		fi
-		sleep 1
-	done
-	kill_dpdk_test
+	result=$(grep "$pattern" $TEST_LOG | awk -F '=' '{print $8}' | sort -g | tail -n1)
+	echo "Result=$result"
+
+	ref_value=$(<$ref_file)
+	expected=$(($ref_value * (100 - $TOLERANCE) / 100))
+	echo "Reference File=$ref_file"
+	echo "Reference Value=$ref_value"
+	echo "Tolerance=${TOLERANCE}%"
+	echo "Expected=$expected"
 
 	# compare results
-	if (( $(echo "$result < ($expected * (100 - $TOLERANCE) / 100)" | bc -l) )); then
-		echo "test_mempool_perf failed: result is too low ($result < $expected)"
+	if [[ $result -lt $expected ]]; then
+		echo "CNXK Mempool Perf Failed"
 		exit 1
 	fi
-	echo "test_mempool_perf passed"
+	echo "CNXK Mempool Perf Passed"
 }
 
-test_mempool_perf
+function get_ref_file()
+{
+	local pn_106xx="0xd49"
+	local pn_96xx="0xd49"
+	local pn
+	local rclk
+	local sclk
+	local div=1000000
+
+	pn=$(grep -m 1 'CPU part' /proc/cpuinfo | awk -F': ' '{print $2}')
+	if [[ $pn == $pn_106xx ]]; then
+		soc="cn106xx"
+		rclk=$(cat /sys/kernel/debug/clk/coreclk/clk_rate)
+	else
+		soc="cn96xx"
+		rclk=$(cat /sys/kernel/debug/clk/rclk/clk_rate)
+	fi
+	rclk=$((rclk / div))
+
+	sclk=$(cat /sys/kernel/debug/clk/sclk/clk_rate)
+	sclk=$((sclk / div))
+	echo "ref_numbers/${soc}_rclk${rclk}_sclk${sclk}"
+}
+
+ref_file=$(get_ref_file)
+test_mempool_perf $ref_file
