a045cfc82e129c14b9b0d76dbf6f77a51f0b9df6
Tejasree Kondoj <ktejasree@marvell.com>
2022-12-09 15:45:30 +0530

    examples/ipsec-secgw: set AES-CTR IV length to 16
    Set AES-CTR IV length as 16 instead of taking from
    SA config option since the application populates
    16B IV in the datapath. AES-CTR requires 16B IV
    constructed from nonce and counter.
    
    Signed-off-by: Tejasree Kondoj <ktejasree@marvell.com>
    Change-Id: Icfabae31fc3a985d750ffc2508e1ace3ae3c0844
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/92193
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>

--- examples/ipsec-secgw/sa.c
+++ examples/ipsec-secgw/sa.c
@@ -1319,8 +1319,13 @@ sa_add_rules(struct sa_ctx *sa_ctx, const struct ipsec_sa entries[],
 			case RTE_CRYPTO_CIPHER_NULL:
 			case RTE_CRYPTO_CIPHER_3DES_CBC:
 			case RTE_CRYPTO_CIPHER_AES_CBC:
-			case RTE_CRYPTO_CIPHER_AES_CTR:
 				iv_length = sa->iv_len;
+				break;
+			case RTE_CRYPTO_CIPHER_AES_CTR:
+				/* Length includes 8B per packet IV, 4B nonce and
+				 * 4B counter as populated in datapath.
+				 */
+				iv_length = 16;
 				break;
 			default:
 				RTE_LOG(ERR, IPSEC_ESP,
