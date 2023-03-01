58e5badf505256ecafc56663a4fa4797486f8cea
Nithin Dabilpuram <ndabilpuram@marvell.com>
2023-03-01 23:37:29 +0530

    common/cnxk: sync from 22.11
    Sync from 22.11
    
    Change-Id: I750a2427ce27c329b39d1082f52dabb70ad41350
    Signed-off-by: Nithin Dabilpuram <ndabilpuram@marvell.com>

--- drivers/net/cnxk/cnxk_ethdev.c
+++ drivers/net/cnxk/cnxk_ethdev.c
@@ -487,7 +487,6 @@ cnxk_nix_tx_queue_setup(struct rte_eth_dev *eth_dev, uint16_t qid,
 	sq->qid = qid;
 	sq->nb_desc = nb_desc;
 	sq->max_sqe_sz = nix_sq_max_sqe_sz(dev);
-	sq->fc_hyst_bits = 0x1;
 
 	if (nix->tx_compl_ena) {
 		sq->cqid = sq->qid + dev->nb_rxq;
