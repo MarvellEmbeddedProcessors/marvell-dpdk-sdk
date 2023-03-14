3c5027eb2f8bf133ef1d84bf77f1495be361cbe5
Nithin Dabilpuram <ndabilpuram@marvell.com>
2023-03-14 12:16:46 +0530

    common/cnxk: ignore error on mode set similar to pfc
    Ignore error reported for other PF/VF using FC similar to PFC.
    Also skip calling mode set if it is already in same state.
    
    Change-Id: I77b1bc40572f9274a1f1d5ab3d5dea95bf2cceda
    Signed-off-by: Nithin Dabilpuram <ndabilpuram@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/99324
    Reviewed-by: Devapraba Muthumani <dmuthumani@marvell.com>
    Tested-by: Devapraba Muthumani <dmuthumani@marvell.com>

--- drivers/common/cnxk/roc_nix_fc.c
+++ drivers/common/cnxk/roc_nix_fc.c
@@ -383,7 +383,7 @@ roc_nix_fc_mode_set(struct roc_nix *roc_nix, enum roc_nix_fc_mode mode)
 	struct nix *nix = roc_nix_to_nix_priv(roc_nix);
 	struct dev *dev = &nix->dev;
 	struct mbox *mbox = mbox_get(dev->mbox);
-	struct cgx_pause_frm_cfg *req, *rsp;
+	struct cgx_pause_frm_cfg *req;
 	uint8_t tx_pause, rx_pause;
 	int rc = -ENOSPC;
 
@@ -398,18 +398,6 @@ roc_nix_fc_mode_set(struct roc_nix *roc_nix, enum roc_nix_fc_mode mode)
 		goto exit;
 	}
 
-	/* Skip set if already in same state to avoid errors */
-	req = mbox_alloc_msg_cgx_cfg_pause_frm(mbox);
-	if (req == NULL)
-		goto exit;
-
-	rc = mbox_process_msg(mbox, (void *)&rsp);
-	if (rc)
-		goto exit;
-
-	if (rsp->rx_pause == rx_pause && rsp->tx_pause == tx_pause)
-		goto skip_set;
-
 	/* Set new config */
 	req = mbox_alloc_msg_cgx_cfg_pause_frm(mbox);
 	if (req == NULL)
@@ -422,10 +410,8 @@ roc_nix_fc_mode_set(struct roc_nix *roc_nix, enum roc_nix_fc_mode mode)
 	if (rc)
 		goto exit;
 
-skip_set:
 	nix->rx_pause = rx_pause;
 	nix->tx_pause = tx_pause;
-
 exit:
 	mbox_put(mbox);
 	return rc;
