37f0b69ccf3ecab617c6c4f621f4d97f554adbc2
Nithin Dabilpuram <ndabilpuram@marvell.com>
2023-06-04 20:19:37 +0530

    ci: msns: add vlan id for pcap gen
    Add VLAN id option for pcap generated for ipsec msns perf
    to work with Cisco profile where DMAC doesn't exist.
    
    Change-Id: I8c8f5c3c28af4e11d411774b82df7a120da7ce49
    Signed-off-by: Nithin Dabilpuram <ndabilpuram@marvell.com>

--- marvell-ci/test/cnxk-tests/ipsec_msns/gentraffic.py
+++ marvell-ci/test/cnxk-tests/ipsec_msns/gentraffic.py
@@ -14,6 +14,8 @@ parser.add_argument("--spi", type=str, help="SPI of ESP packet",
 		    default=None)
 parser.add_argument("--dmac", type=str, help="DMAC of pkts",
 		    default=None)
+parser.add_argument("--vlan-id", type=int, help="VLAN id",
+		    default=None)
 parser.add_argument("--name", type=str, help="Name of pcap",
 		    default=None)
 
@@ -22,6 +24,7 @@ if args.spi != None:
 	spi = int(args.spi, 16)
 if args.dmac != None:
 	dmac = args.dmac
+vlan_id = args.vlan_id
 
 key = b'\xfe\xff\xe9\x92\x86\x65\x73\x1c\x6d\x6a\x8f\x94\x67\x30\x83\x08\xca\xfe\xba\xbe'
 eth = Ether(src='12:12:12:12:12:12', dst=dmac)
@@ -37,7 +40,10 @@ ip=IP(src='11.11.11.1', dst='11.11.11.2')
 
 p=ip/TCP()/Raw(5 * 'a')
 
-pkt=Ether(src='12:12:12:12:12:12', dst=dmac)/sa.encrypt(p)
+if vlan_id != None:
+	pkt=Ether(src='12:12:12:12:12:12', dst=dmac)/Dot1Q(vlan=int(vlan_id))/sa.encrypt(p)
+else:
+	pkt=Ether(src='12:12:12:12:12:12', dst=dmac)/sa.encrypt(p)
 
 if args.name != None:
 	pcap_name = args.name
@@ -45,4 +51,7 @@ else:
 	pcap_name = 'inb_%08x.pcap' % spi
 wrpcap(pcap_name, pkt)
 
-sys.stdout.write("Generated %s with SPI %08x\n" % (pcap_name, spi))
+if vlan_id != None:
+	sys.stdout.write("Generated %s with SPI %08x with VLAN %u\n" % (pcap_name, spi, vlan_id))
+else:
+	sys.stdout.write("Generated %s with SPI %08x\n" % (pcap_name, spi))
