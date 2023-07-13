991ddf422f18336be772dd6bd5d5373570009466
Vamsi Attunuru <vattunuru@marvell.com>
2023-07-12 23:11:54 -0700

    dma/cnxk: sync with 23.03
    sync with 23.03-devel driver
    
    Signed-off-by: Vamsi Attunuru <vattunuru@marvell.com>
    Change-Id: I7459aea0e0b81d16d0bdc6f2f7bec7d3a745c87e
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/107453
    Base-Builds: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Base-Tests: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Jerin Jacob Kollanukkaran <jerinj@marvell.com>

--- drivers/dma/cnxk/cnxk_dmadev.h
+++ drivers/dma/cnxk/cnxk_dmadev.h
@@ -4,17 +4,22 @@
 #ifndef CNXK_DMADEV_H
 #define CNXK_DMADEV_H
 
-#define DPI_MAX_POINTER		15
-#define DPI_QUEUE_STOP		0x0
-#define DPI_QUEUE_START		0x1
-#define STRM_INC(s)		((s).tail = ((s).tail + 1) % (s).max_cnt)
-#define DPI_MAX_DESC		1024
-#define MAX_VCHANS_PER_QUEUE	4
+#include <roc_api.h>
+
+#define DPI_MAX_POINTER	     15
+#define STRM_INC(s, var)     ((s).var = ((s).var + 1) & (s).max_cnt)
+#define STRM_DEC(s, var)     ((s).var = ((s).var - 1) == -1 ? (s).max_cnt : ((s).var - 1))
+#define DPI_MAX_DESC	     1024
+#define DPI_MIN_DESC	     2
+#define MAX_VCHANS_PER_QUEUE 4
 
 /* Set Completion data to 0xFF when request submitted,
  * upon successful request completion engine reset to completion status
  */
-#define DPI_REQ_CDATA		0xFF
+#define DPI_REQ_CDATA 0xFF
+
+#define CNXK_DPI_DEV_CONFIG (1ULL << 0)
+#define CNXK_DPI_DEV_START  (1ULL << 1)
 
 struct cnxk_dpi_compl_s {
 	uint64_t cdata;
@@ -22,7 +27,7 @@ struct cnxk_dpi_compl_s {
 };
 
 struct cnxk_dpi_cdesc_data_s {
-	struct cnxk_dpi_compl_s *compl_ptr[DPI_MAX_DESC];
+	struct cnxk_dpi_compl_s **compl_ptr;
 	uint16_t max_cnt;
 	uint16_t head;
 	uint16_t tail;
@@ -31,15 +36,18 @@ struct cnxk_dpi_cdesc_data_s {
 struct cnxk_dpi_conf {
 	union dpi_instr_hdr_s hdr;
 	struct cnxk_dpi_cdesc_data_s c_desc;
+	uint16_t pnum_words;
+	uint16_t pending;
+	uint16_t desc_idx;
+	uint16_t pad0;
+	struct rte_dma_stats stats;
 };
 
 struct cnxk_dpi_vf_s {
 	struct roc_dpi rdpi;
 	struct cnxk_dpi_conf conf[MAX_VCHANS_PER_QUEUE];
-	struct rte_dma_stats stats;
-	uint64_t cmd[DPI_MAX_CMD_SIZE];
-	uint32_t num_words;
-	uint16_t desc_idx;
-};
+	uint16_t num_vchans;
+	uint16_t flag;
+} __plt_cache_aligned;
 
 #endif
