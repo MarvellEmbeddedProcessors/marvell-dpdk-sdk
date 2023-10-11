4f2b1f413f77786a279135cbd3380748321ee7bd
Tejasree Kondoj <ktejasree@marvell.com>
2023-10-11 11:12:34 +0530

    crypto/cnxk: fix RISC-V compilation
    Fixing RISC-V compilation observed with upstream.
    
    Signed-off-by: Tejasree Kondoj <ktejasree@marvell.com>
    Change-Id: I009e2b43c8f011abec6a9901bac4f4dc8745019e
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/113567
    Base-Builds: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Base-Tests: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>
    (cherry picked from commit 8b6142be8a8b3141ca18e2c1247a5fdf21176b1a)
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/113856

--- drivers/crypto/cnxk/cnxk_se.h
+++ drivers/crypto/cnxk/cnxk_se.h
@@ -1718,6 +1718,10 @@ cpt_kasumi_enc_prep(uint32_t req_flags, uint64_t d_offs, uint64_t d_lens,
 	cpt_inst_w4.s.param2 = auth_data_len;
 
 	inst->w4.u64 = cpt_inst_w4.u64;
+
+	if (unlikely(iv_s == NULL))
+		return -1;
+
 	if (is_sg_ver2)
 		sg2_inst_prep(params, inst, offset_ctrl, iv_s, iv_len, 0, 0, inputlen, outputlen, 0,
 			      req_flags, 0, 0);
@@ -1774,6 +1778,10 @@ cpt_kasumi_dec_prep(uint64_t d_offs, uint64_t d_lens, struct roc_se_fc_params *p
 	}
 
 	inst->w4.u64 = cpt_inst_w4.u64;
+
+	if (unlikely(params->iv_buf == NULL))
+		return -1;
+
 	if (is_sg_ver2)
 		sg2_inst_prep(params, inst, offset_ctrl, params->iv_buf, iv_len, 0, 0, inputlen,
 			      outputlen, 0, 0, 0, 1);
