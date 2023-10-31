afe1ea42186e2751f901a35fead1ad9cf8888b15
Pavan Nikhilesh <pbhagavatula@marvell.com>
2023-10-31 22:02:31 +0530

    net/octeon_ep: sync from 23.07
    Signed-off-by: Pavan Nikhilesh <pbhagavatula@marvell.com>
    Change-Id: I1766f422bfb1e7de4b87730e1ec2f78efc4e1fda

--- drivers/net/octeon_ep/otx_ep_common.h
+++ drivers/net/octeon_ep/otx_ep_common.h
@@ -394,11 +394,11 @@ struct otx_ep_droq {
 	 */
 	void *pkts_sent_reg;
 
-	/** Fix for DMA incompletion during pkt reads.
-	 *  This variable is used to initiate a sent_reg_read
-	 *  that completes pending dma
-	 *  this variable is used as lvalue so compiler cannot optimize
-	 *  the reads
+	/** Handle DMA incompletion during pkt reads.
+	 * This variable is used to initiate a sent_reg_read
+	 * that completes pending dma
+	 * this variable is used as lvalue so compiler cannot optimize
+	 * the reads.
 	 */
 	uint32_t sent_reg_val;
 
@@ -558,9 +558,6 @@ struct otx_ep_device {
 
 	/* Negotiated Mbox version */
 	uint32_t mbox_neg_ver;
-
-	/* firmware info */
-	struct otx_ep_fw_info fw_info;
 };
 
 int otx_ep_setup_iqs(struct otx_ep_device *otx_ep, uint32_t iq_no,
