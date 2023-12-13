d420ed1ea9c6af4871580f8e8b6082419a4edf59
Harman Kalra <hkalra@marvell.com>
2023-12-14 02:13:48 +0530

    common/cnxk: representee notification callback
    Setting up a callback which gets invoked every time a representee
    comes up or goes down. Later this callback gets handled by network
    conterpart.
    
    Change-Id: Iacbdbda819b19426a08626b1061ffa53b02d3029
    Signed-off-by: Harman Kalra <hkalra@marvell.com>

--- drivers/common/cnxk/roc_eswitch.h
+++ drivers/common/cnxk/roc_eswitch.h
@@ -8,6 +8,9 @@
 #define ROC_ESWITCH_VLAN_TPID 0x8100
 #define ROC_ESWITCH_LBK_CHAN  63
 
+/* Process representee notification callback */
+typedef int (*process_repte_notify_t)(void *roc_nix, uint16_t pf_func, bool enable);
+
 /* NPC */
 int __roc_api roc_eswitch_npc_mcam_rx_rule(struct roc_npc *roc_npc, struct roc_npc_flow *flow,
 					   uint16_t pcifunc, uint16_t vlan_tci,
@@ -23,4 +26,7 @@ int __roc_api roc_eswitch_npc_rss_action_configure(struct roc_npc *roc_npc,
 /* NIX */
 int __roc_api roc_eswitch_nix_vlan_tpid_set(struct roc_nix *nix, uint32_t type, uint16_t tpid,
 					    bool is_vf);
+int __roc_api roc_eswitch_nix_process_repte_notify_cb_register(struct roc_nix *roc_nix,
+						    process_repte_notify_t proc_repte_nt);
+void __roc_api roc_eswitch_nix_process_repte_notify_cb_unregister(struct roc_nix *roc_nix);
 #endif /* __ROC_ESWITCH_H__ */
