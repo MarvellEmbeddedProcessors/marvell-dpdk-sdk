89477477997b1456bb505e78c6edb89acdc6fa46
Srikanth Yalavarthi <syalavarthi@marvell.com>
2023-12-18 05:29:14 -0800

    buildtools: remove absolute paths from pc file
    When linking with non-versioned libraries, absolute paths
    of the libraries are added to libdpdk.pc. This patch replaces
    the absolute path with correct linker flags, -l<libname>.
    
    https://github.com/mesonbuild/meson/issues/7766
    
    Signed-off-by: Srikanth Yalavarthi <syalavarthi@marvell.com>
    Change-Id: Iee38e9e905719a9f43a7a2df1fe0311fe4f8e617
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/167937
    Base-Builds: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Shivah Shankar Shankar Narayan Rao <sshankarnara@marvell.com>
    Tested-by: Shivah Shankar Shankar Narayan Rao <sshankarnara@marvell.com>

--- buildtools/pkg-config/set-static-linker-flags.py
+++ buildtools/pkg-config/set-static-linker-flags.py
@@ -6,9 +6,12 @@
 # Should be called from meson build itself
 import os
 import sys
+import re
 
 
 def fix_ldflag(f):
+    if (f.startswith('/') and (f.endswith('.so') or re.search(r".so.\*$", f) or f.endswith('.a'))):
+        return f.split('/', -1)[-1].split('.', -1)[0].replace('lib', '-l', 1)
     if not f.startswith('-lrte_'):
         return f
     return '-l:lib' + f[2:] + '.a'
