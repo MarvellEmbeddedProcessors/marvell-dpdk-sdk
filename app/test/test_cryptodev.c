725fe6dea5aa4ff9b76f57c8a2576a77fc5d9bc9
Vidya Sagar Velumuri <vvelumuri@marvell.com>
2024-11-27 01:29:22 -0800

    test/crypto: add autotest support for cn20k
    Add crypto autotest support for cn20k
    
    Signed-off-by: Vidya Sagar Velumuri <vvelumuri@marvell.com>
    Change-Id: I183c3cde30916750e80258acaea61e6e832a8eae
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/140181
    Base-Builds: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/146472

--- app/test/test_cryptodev.c
+++ app/test/test_cryptodev.c
@@ -21008,6 +21008,18 @@ test_cryptodev_cn10k_raw_api(void)
 }
 
 static int
+test_cryptodev_cn20k(void)
+{
+	return run_cryptodev_testsuite(RTE_STR(CRYPTODEV_NAME_CN20K_PMD));
+}
+
+static int
+test_cryptodev_cn20k_raw_api(void)
+{
+	return run_cryptodev_raw_testsuite(RTE_STR(CRYPTODEV_NAME_CN20K_PMD));
+}
+
+static int
 test_cryptodev_dpaa2_sec_raw_api(void)
 {
 	return run_cryptodev_raw_testsuite(RTE_STR(CRYPTODEV_NAME_DPAA2_SEC_PMD));
@@ -21062,4 +21074,7 @@ REGISTER_DRIVER_TEST(cryptodev_nitrox_autotest, test_cryptodev_nitrox);
 REGISTER_DRIVER_TEST(cryptodev_bcmfs_autotest, test_cryptodev_bcmfs);
 REGISTER_DRIVER_TEST(cryptodev_cn9k_autotest, test_cryptodev_cn9k);
 REGISTER_DRIVER_TEST(cryptodev_cn10k_autotest, test_cryptodev_cn10k);
+REGISTER_DRIVER_TEST(cryptodev_cn20k_autotest, test_cryptodev_cn20k);
+REGISTER_DRIVER_TEST(cryptodev_cn20k_raw_api_autotest,
+		test_cryptodev_cn20k_raw_api);
 REGISTER_DRIVER_TEST(cryptodev_zsda_autotest, test_cryptodev_zsda);
