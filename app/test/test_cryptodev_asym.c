8d89672d72b96208d94b49c1ff5affb32454f7be
Vidya Sagar Velumuri <vvelumuri@marvell.com>
2025-02-19 03:14:34 -0800

    test/crypto: add asym autotest support for cn20k
    Add crypto asym autotest support for cn20k
    
    Signed-off-by: Vidya Sagar Velumuri <vvelumuri@marvell.com>
    Change-Id: Ib280a3df4b0bd811bbee514b9054de5ccb758050
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/146377
    Base-Builds: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Base-Tests: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>

--- app/test/test_cryptodev_asym.c
+++ app/test/test_cryptodev_asym.c
@@ -3709,8 +3709,23 @@ test_cryptodev_cn10k_asym(void)
 	return unit_test_suite_runner(&cryptodev_octeontx_asym_testsuite);
 }
 
+static int
+test_cryptodev_cn20k_asym(void)
+{
+	gbl_driver_id = rte_cryptodev_driver_id_get(
+			RTE_STR(CRYPTODEV_NAME_CN20K_PMD));
+	if (gbl_driver_id == -1) {
+		RTE_LOG(ERR, USER1, "CN20K PMD must be loaded.\n");
+		return TEST_SKIPPED;
+	}
+
+	/* Use test suite registered for crypto_octeontx PMD */
+	return unit_test_suite_runner(&cryptodev_octeontx_asym_testsuite);
+}
+
 REGISTER_DRIVER_TEST(cryptodev_openssl_asym_autotest, test_cryptodev_openssl_asym);
 REGISTER_DRIVER_TEST(cryptodev_qat_asym_autotest, test_cryptodev_qat_asym);
 REGISTER_DRIVER_TEST(cryptodev_octeontx_asym_autotest, test_cryptodev_octeontx_asym);
 REGISTER_DRIVER_TEST(cryptodev_cn9k_asym_autotest, test_cryptodev_cn9k_asym);
 REGISTER_DRIVER_TEST(cryptodev_cn10k_asym_autotest, test_cryptodev_cn10k_asym);
+REGISTER_DRIVER_TEST(cryptodev_cn20k_asym_autotest, test_cryptodev_cn20k_asym);
