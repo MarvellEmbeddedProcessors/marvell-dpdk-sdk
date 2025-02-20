af447644bc76453ad48c4404e1ad0890ccacd40a
Nithinsen Kaithakadan <nkaithakadan@marvell.com>
2025-02-20 15:06:00 +0000

    common/cnxk: fix salt handling with aes-ctr
    This patch includes fix for setting correct salt value
    for CTR algorithm.
    
    Fixes: 78d03027f2cc ("common/cnxk: add IPsec common code")
    
    Signed-off-by: Nithinsen Kaithakadan <nkaithakadan@marvell.com>
    Change-Id: I5a1fbca47de3fe60c9587f48f7728f3187120243
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/146488
    Base-Builds: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Base-Tests: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Anoob Joseph <anoobj@marvell.com>
    (cherry picked from commit b4e0555803a81cb7c441c39b69c6b89d44030fea)
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/146699

--- drivers/common/cnxk/cnxk_security.c
+++ drivers/common/cnxk/cnxk_security.c
@@ -95,6 +95,9 @@ ot_ipsec_sa_common_param_fill(union roc_ot_ipsec_sa_word2 *w2, uint8_t *cipher_k
 				break;
 			case RTE_CRYPTO_CIPHER_AES_CTR:
 				w2->s.enc_type = ROC_IE_SA_ENC_AES_CTR;
+				memcpy(salt_key, &ipsec_xfrm->salt, 4);
+				tmp_salt = (uint32_t *)salt_key;
+				*tmp_salt = rte_be_to_cpu_32(*tmp_salt);
 				break;
 			case RTE_CRYPTO_CIPHER_3DES_CBC:
 				w2->s.enc_type = ROC_IE_SA_ENC_3DES_CBC;
