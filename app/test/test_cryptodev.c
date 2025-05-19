b3bcf290bd74db051af343db45bf6c87de8a5c34
Nithinsen Kaithakadan <nkaithakadan@marvell.com>
2025-05-19 05:27:31 +0000

    test/crypto: set ptr to null post crypto op free
    Set pointer to NULL post rte_crypto_op_free operation.
    
    Fixes: dc0cdbdec1c ("test/crypto: add QP depth used count case")
    Fixes: 202d375c60b ("app/test: add cryptodev unit and performance tests")
    Fixes: ffbe3be0d4b ("app/test: add libcrypto")
    
    Signed-off-by: Nithinsen Kaithakadan <nkaithakadan@marvell.com>
    Change-Id: I48783a9176cc345579ea0a6a41cd3e6fc21baaea
    Reviewed-on: https://sj1git1.cavium.com/c/IP/SW/dataplane/dpdk/+/153385
    Base-Builds: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Base-Tests: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Tested-by: sa_ip-toolkits-Jenkins <sa_ip-toolkits-jenkins@marvell.com>
    Reviewed-by: Akhil Goyal <gakhil@marvell.com>
    Reviewed-by: Tejasree Kondoj <ktejasree@marvell.com>
    Reviewed-by: Jerin Jacob <jerinj@marvell.com>

--- app/test/test_cryptodev.c
+++ app/test/test_cryptodev.c
@@ -2592,6 +2592,7 @@ test_queue_pair_descriptor_count(void)
 
 		rte_pktmbuf_free(ops_deq[i]->sym->m_src);
 		rte_crypto_op_free(ops_deq[i]);
+		ops_deq[i] = NULL;
 	}
 
 	return TEST_SUCCESS;
@@ -14781,6 +14782,7 @@ test_multi_session(void)
 
 		/* free crypto operation structure */
 		rte_crypto_op_free(ut_params->op);
+		ut_params->op = NULL;
 
 		/*
 		 * free mbuf - both obuf and ibuf are usually the same,
@@ -14926,6 +14928,7 @@ test_multi_session_random_usage(void)
 					ut_paramz[j].iv);
 
 		rte_crypto_op_free(ut_paramz[j].ut_params.op);
+		ut_paramz[j].ut_params.op = NULL;
 
 		/*
 		 * free mbuf - both obuf and ibuf are usually the same,
